a90f55675f117221bf7cf91531b817d126b2fc41
Michael <student@epicodus.com>
2015-06-25 12:06:42 -0700

    Nav css and listing css

--- app/controllers/listing.js
+++ app/controllers/listing.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  toilets: ['Yes', 'No'],
-  showers: ['Yes', 'No'],
-  outlets: ['Yes', 'No'],
-  pets: ['Yes', 'No'],
-  avgRating: [1,2], // remove this later
+  toilets: ['yes', 'no'],
+  showers: ['yes', 'no'],
+  outlets: ['yes', 'no'],
+  pets: ['yes', 'no'],
+  avgRating: [0],
+  totalStars: [0],
   isEditing: false,
   needs: ['map', 'listing'],
   actions: {
@@ -27,6 +28,7 @@ export default Ember.Controller.extend({
       listing.set('image2', this.get('model.image2'));
       listing.set('image3', this.get('model.image3'));
       listing.set('image4', this.get('model.image4'));
+      listing.set('totalStars', this.get('model.totalStars'));
       listing.save();
       this.set('isEditing', false);
       this.transitionToRoute('listings');
@@ -46,12 +48,16 @@ export default Ember.Controller.extend({
       });
       var listing = this.get('controllers.listing.model');
       var stars = parseInt(this.get('stars'));
-      var newAvgRating = parseInt(listing.get('avgRating'));
+      var avgRating = parseInt(this.get('avgRating'));
+      var totalReviews = this.store.all('review').get('length');
+      debugger;
       review.save().then(function() {
+        listing.get('totalStars').pushObject(stars);
         listing.get('reviews').pushObject(review);
-        newAvgRating = Math.floor((newAvgRating + stars) / 2);
+        var newAvgRating = Math.floor((avgRating + stars) / totalReviews);
         listing.set('avgRating', newAvgRating);
         listing.save();
+        return newAvgRating;
       });
 
       $("#one").hide();
